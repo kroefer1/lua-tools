ea3bc5387025f67573000d700640caa70af13add
kroefer1 <thegamingsnake19@gmail.com>
2026-07-18 17:26:54 +0200

    add main functions and example file

---
+++ main.lua
@@ -0,0 +1,36 @@
+-- epic lua coding by kroefer (human generated)
+-- modules
+local sleep = require("sleep")
+local base64 = require("encode")
+
+-- function that prints text and adds a delay
+local function wPrint(text, delay)
+print(text)
+sleep(delay)
+end
+
+-- experimental base64 encoder (encode.lua in root of project)
+local encoded = base64("test")
+print(encoded)
+
+-- code
+wPrint("dont kys gng :heart:", 3)
+print(" ")
+wPrint("the nt file system (ntfs) was designed for microslop windows and user friendlyness n shi idk", 5)
+print(" ")
+wPrint("cool right?", 3)
+wPrint("no? you dint find it cool?", 3)
+wPrint("well fuck you my shit is cool", 3)
+print("oh well cya")
+
+--[[
+yo vro
+
+decode ts shi
+you may be wondering "why the fuck did you encode this"
+the reason is uhh
+uhhhh...
+idk im feelin funny
+
+eW8KbGlmZSBpcyB1bmZhaXIKc29tZXRpbWVzIGxpZmUgcHJvbWlzZXMgcmV3YXJkCmJ1dCBubyByZXdhcmQKbGlrZSBjYXNpbm8KYnV0IHRoYXRzIG5vdCBzaWduIHRvIGdpdmUgdXAKdGhhdCBzaWduIHRvIGNvbnRpbnVlCmlmIHlvdSBnaXZlIHVwIHJpZ2h0IGJlZm9yZSBqYWNrcG90CnlvdSBuZXZlciB3aW4gamFja3BvdAo=
+]]--
